a12202a1d2e5e8ae32109d07721112d7d4ef342f
Noah <noah_mcclung@icloud.com>
2026-05-16 14:11:31 -0700

    Initial Commit

---
+++ Haze/HazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HazeApp.swift
+//  Haze
+//
+//  Created by Noah McClung on 5/16/26.
+//
+
+import SwiftUI
+
+@main
+struct HazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
