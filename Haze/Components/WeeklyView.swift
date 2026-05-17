7392587e6723c93134517f6f3ee3721516f3ce3e
Noah <noah_mcclung@icloud.com>
2026-05-16 21:11:49 -0700

    Initial Commit

---
+++ Haze/Components/WeeklyView.swift
@@ -0,0 +1,18 @@
+//
+//  WeeklyView.swift
+//  Haze
+//
+//  Created by Noah McClung on 5/16/26.
+//
+
+import SwiftUI
+
+struct WeeklyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeeklyView()
+}
---
+++ Haze/Views/DashboardView.swift
@@ -0,0 +1,18 @@
+//
+//  DashboardView.swift
+//  Haze
+//
+//  Created by Noah McClung on 5/16/26.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DashboardView()
+}
